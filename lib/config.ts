c697f1166e65e00a30450967caaffe56ed20b1ba
SangEon <sup8369@gmail.com>
2023-09-01 14:56:28 +0900

    fix: url path

--- lib/config.ts
+++ lib/config.ts
@@ -96,7 +96,7 @@ export const isPreviewImageSupportEnabled: boolean = getSiteConfig(
 // Optional whether or not to include the Notion ID in page URLs or just use slugs
 export const includeNotionIdInUrls: boolean = getSiteConfig(
   'includeNotionIdInUrls',
-  !!isDev
+  true
 )
 
 export const navigationStyle: NavigationStyle = getSiteConfig(
